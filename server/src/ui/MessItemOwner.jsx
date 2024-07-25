5a36ca1bcb09376496d92f2e7df62d2fb0a21a96
HrishikeshChaudhari24 <chaudharihrishikesh30@gmail.com>
2024-07-25 15:35:23 +0530

    Update MessItemOwner.jsx

--- server/src/ui/MessItemOwner.jsx
+++ server/src/ui/MessItemOwner.jsx
@@ -32,7 +32,7 @@ const MessItemOwner = (listing) => {
 
 
     const deleteListing = async (id) => {
-        axios.delete(`http://localhost:3000/listings/${id}`);
+        axios.delete(`https://tastetrove.onrender.com/listings/${id}`);
         // getData()
     };
 
