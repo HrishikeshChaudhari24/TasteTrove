05182692bc95d264017d808d6ffe4e4f6375e12f
HrishikeshChaudhari24 <chaudharihrishikesh30@gmail.com>
2024-07-25 15:16:48 +0530

    Update SignUpForm.jsx

--- server/src/features/user/SignInForm/SignUpForm.jsx
+++ server/src/features/user/SignInForm/SignUpForm.jsx
@@ -17,12 +17,12 @@ const SignInForm = () => {
   // Function to handle Google sign-in
   const handleGoogleSignIn = () => {
     // Implement the logic for Google Sign-In here
-    window.open("http://localhost:3000/auth/google", "_self");
+    window.open("https://tastetrove.onrender.com/auth/google", "_self");
     console.log("Google Sign-In clicked");
   };
   const handleAdminGoogleSignIn = () => {
     // Implement the logic for Google Sign-In here
-    window.open("http://localhost:3000/admin/auth/google", "_self");
+    window.open("https://tastetrove.onrender.com/admin/auth/google", "_self");
     console.log("Google Admin Sign-In clicked");
   };
   // useEffect(() => {
@@ -61,7 +61,7 @@ const SignInForm = () => {
       {credentials.selectValue == "Customer" ? (
         <>
           <form
-            action="http://localhost:3000/register"
+            action="https://tastetrove.onrender.com/register"
             method="post"
             className="form"
           >
@@ -147,7 +147,7 @@ const SignInForm = () => {
       ) : credentials.selectValue == "MessOwner" ? (
         <>
           <form
-            action="http://localhost:3000/admin/register"
+            action="https://tastetrove.onrender.com/admin/register"
             method="post"
             className="form"
           >
