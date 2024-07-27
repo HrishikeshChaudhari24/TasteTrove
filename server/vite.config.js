395208529a8ae64f59b4117734d8f418c189dbee
HrishikeshChaudhari24 <chaudharihrishikesh30@gmail.com>
2024-07-27 07:26:12 +0530

    Update vite.config.js

--- server/vite.config.js
+++ server/vite.config.js
@@ -1,11 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { visualizer } from 'rollup-plugin-visualizer';
+import ImageminPlugin from 'vite-plugin-imagemin';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-   server: {
-    host: '0.0.0.0', // Bind to all network interfaces
-    // port: 3000       // Optional: Specify the port
-  }
-})
+  plugins: [
+    react(),
+    createHtmlPlugin(),
+    visualizer({ open: true }),
+    ImageminPlugin({
+      // configure options here, e.g., for PNG optimization
+      pngquant: {
+        quality: [0.65, 0.9],
+      },
+    }),
+    CompressionPlugin({
+      verbose: true,
+      threshold: 10240,
+      algorithm: 'gzip',
+      minRatio: 0.8,
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      // Customize Rollup options here if needed
+    },
+  },
+});
