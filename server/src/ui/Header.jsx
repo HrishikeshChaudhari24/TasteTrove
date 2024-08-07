5ef24a2adcfd3b6a7896dee6f142a0950a96c572
HrishikeshChaudhari24 <chaudharihrishikesh30@gmail.com>
2024-08-07 19:30:45 +0530

    Update Header.jsx

--- server/src/ui/Header.jsx
+++ server/src/ui/Header.jsx
@@ -1,21 +1,18 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
 import Username from "../features/user/Username";
 import { FaUser, FaSignOutAlt } from "react-icons/fa";
 import { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { getAddress } from "../services/apiGeocoding";
+import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import { data } from "autoprefixer";
 import { addUser, removeUser } from "./store/user";
-import { useSelector } from "react-redux";
+
 function Header() {
   const [userAddress, setUserAddress] = useState("");
-  const userdata = useSelector((state) => state.userdata);
   const [userData, setUserData] = useState({});
-  // console.log("userdata:",userData);
-
+  const userdata = useSelector((state) => state.userdata);
   const dispatch = useDispatch();
+
   const fetchUserAddress = async () => {
     try {
       const address = await getAddress(); // Replace with your actual API call to fetch user address
@@ -27,7 +24,6 @@ function Header() {
 
   function logout() {
     window.open("https://tastetrove.onrender.com/auth/logout", "_self");
-    // userData=""
     removeUserData(userData);
     setUserData("");
   }
@@ -36,69 +32,53 @@ function Header() {
     axios
       .get("https://tastetrove.onrender.com/users", { withCredentials: true })
       .then((response) => {
-         console.log(response.data)
+        console.log(response.data);
         setUserData(response.data);
-         // dispatch(addUser(response.data));
         addUserData(response.data);
       })
       .catch((error) => {
-        console.log("hello")
-        console.log(error);
+        console.error("Error fetching user data", error);
       });
   }, []);
+
   const addUserData = (payload) => {
-    // console.log(payload)
     dispatch(addUser(payload));
   };
+
   const removeUserData = (payload) => {
     dispatch(removeUser(payload));
   };
-  // useEffect(()=>{
 
-  // },[])
   return (
     <header className="flex justify-between items-center border-b border-stone-200 bg-yellow-400 px-4 py-3 sm:px-6">
-      <Link
-        to="/"
-        className="tracking-widest text-xl flex uppercase ml-3 font-extrabold"
-      >
-        Taste<p className="text-red-500 font-extrabold">Trove</p>{" "}
+      <Link to="/" className="tracking-widest text-xl flex uppercase ml-3 font-extrabold">
+        Taste<p className="text-red-500 font-extrabold">Trove</p>
       </Link>
 
       <div className="flex justify-between items-center space-x-16 px-4 font-semibold">
-      <Link to="/">Home</Link>
+        <Link to="/">Home</Link>
         <Link to="/allListings">All Mess</Link>
-        {/* Display Location */}
 
         <SearchOrder />
-        {/* <div className="hidden sm:flex items-center space-x-4">
-          <Username />
-          <Link to="/login">
-            <FaUser className="cursor-pointer" />
-          </Link>
-        </div> */}
 
-        {userdata && userdata.length > 0 && userdata[0].Type == "Admin" && (
-          <button className="">
-            {" "}
-            <a href={`/Adminprofile/${userdata[0]._id}`}>
-              {/* {userdata[0].name.split(" ")[0]} */}{" "}
-              <FaUser className="cursor-pointer" />
-            </a>
-          </button>
-        )}
-        {userdata && userdata.length > 0 && userdata[0].Type == "User" && (
+        {userdata && userdata.length > 0 && userdata[0].Type === "Admin" && (
           <button>
-            {" "}
-            <a href={`/Userprofile/${userdata[0]._id}`}>
-              {/* {userdata[0].name.split(" ")[0]} */}{" "}
+            <a href={`/Adminprofile/${userdata[0]._id}`}>
               <FaUser className="cursor-pointer" />
             </a>
           </button>
         )}
+
+        {userdata && userdata.length > 0 && userdata[0].Type === "User" && (
+          <button>
+            <a href={`/Userprofile/${userdata[0]._id}`}>
+              <FaUser className="cursor-pointer" />
+            </a>
+          </button>
+        )}
+
         {userdata.length === 0 && (
           <div className="hidden sm:flex items-center space-x-4">
-            {/* <Username /> */}
             <Link to="/login" onClick={logout}>
               <FaUser className="cursor-pointer" />
             </Link>
@@ -106,10 +86,7 @@ function Header() {
         )}
 
         {userdata.length > 0 && (
-          <div
-            className="flex items-center space-x-4"
-            style={{ marginTop: "8px", marginRight: "10px" }}
-          >
+          <div className="flex items-center space-x-4" style={{ marginTop: "8px", marginRight: "10px" }}>
             <Username />
             <div className="relative">
               <button className="cursor-pointer" onClick={logout}>
@@ -126,4 +103,3 @@ function Header() {
 }
 
 export default Header;
-// export{logout};
