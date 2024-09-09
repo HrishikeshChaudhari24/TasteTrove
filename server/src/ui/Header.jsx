234c8bc4ac481ed48a393d76837d0ea97b93cabc
HrishikeshChaudhari24 <chaudharihrishikesh30@gmail.com>
2024-09-09 17:33:15 +0530

    Update Header.jsx

--- server/src/ui/Header.jsx
+++ server/src/ui/Header.jsx
@@ -30,7 +30,7 @@ function Header() {
 
   useEffect(() => {
     axios
-      .get("https://tastetrove.onrender.com/auth/user", { withCredentials: true })
+      .get("https://tastetrove.onrender.com/auth/user")
       .then((response) => {
         console.log(response.data);
         setUserData(response.data);
