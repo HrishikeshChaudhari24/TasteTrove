30008583d496250cf1cb012084696d5f715cce2f
HrishikeshChaudhari24 <chaudharihrishikesh30@gmail.com>
2024-09-10 23:38:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -76,23 +76,59 @@ const methodOverride = require("method-override");
 const asyncWrapper = require("./middlewares/async.js");
 app.use(methodOverride("_method"));
 // initializingPassport(passport);
-app.use(
-  expressSession({
-    secret: process.env.secret,
-    saveUninitialized: false,  // Typically, you don't want to save uninitialized sessions
-    resave: false,             // Resave only if the session has changed
-    store: MongoStore.create({
-      mongoUrl: process.env.MONGO_URI,
-      collectionName: "sessions",
-    }),
-    cookie: {
-      maxAge: 24 * 60 * 60 * 1000,  // 1 day, adjust if necessary
-      // httpOnly: true,               // Prevent client-side JavaScript from accessing cookies
-      sameSite: 'none',              // Helps prevent CSRF attacks
-      secure: true,  // Only use secure cookies in production
-    },
-  })
-);
+// app.use(
+//   expressSession({
+//     secret: process.env.secret,
+//     saveUninitialized: false,  // Typically, you don't want to save uninitialized sessions
+//     resave: false,             // Resave only if the session has changed
+//     store: MongoStore.create({
+//       mongoUrl: process.env.MONGO_URI,
+//       collectionName: "sessions",
+//     }),
+//     cookie: {
+//       maxAge: 24 * 60 * 60 * 1000,  // 1 day, adjust if necessary
+//       // httpOnly: true,               // Prevent client-side JavaScript from accessing cookies
+//       sameSite: 'none',              // Helps prevent CSRF attacks
+//       secure: true,  // Only use secure cookies in production
+//     },
+//   })
+// );
+
+
+
+app.set('trust proxy', 1); // Trust the x-forwarded-proto header
+
+// Session configuration with dynamic SameSite and Secure options
+app.use(expressSession({
+  secret: process.env.secret,
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }), // MongoDB store
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+    httpOnly: true, // Prevent access from JavaScript
+    secure: 'auto', // Secure will be determined based on the x-forwarded-proto header
+    sameSite: 'none', // SameSite=None for cookies to be sent in cross-site contexts
+  }
+}));
+
+// Middleware to conditionally set iframe permissions
+app.use((req, res, next) => {
+  const internalPaths = ['/status', '/admin','/users','/auth/users']; // List of internal pages
+  if (internalPaths.includes(req.path)) {
+    // For internal paths, block iframe embedding
+    res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  } else {
+    // For public pages, allow iframe embedding from any origin
+    res.setHeader('X-Frame-Options', 'ALLOWALL');
+  }
+  next();
+});
+
+
+
+
+
 // app.use(
 //   require("cookie-session")({
 //     name: "session",
