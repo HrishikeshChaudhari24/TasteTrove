32f460aceb04182b7dc6f1391a4238cc5d13b386
Hrishikesh Chaudhari <chaudharihrishikesh30@gmail.com>
2025-09-15 21:56:30 +0530

    Update App.jsx

--- server/src/App.jsx
+++ server/src/App.jsx
@@ -29,7 +29,7 @@ import UserProfile from './ui/UserProfile'
 import "./index.css";
 import AdminHub from "./ui/Dashboard"
 import DashboardAdmin from "./ui/DashboardAdmin"
-import NearByMesses from './ui/NearByMesses';
+import NearbyMesses from './ui/NearByMesses';
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
@@ -121,7 +121,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/nearby",
-        element: <NearByMesses />,
+        element: <NearbyMesses />,
         
       }
       // {
