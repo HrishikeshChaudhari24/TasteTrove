7bdc879c5c3ee0206340a2eb69c8cb9f1a460c0b
Hrishikesh Chaudhari <chaudharihrishikesh30@gmail.com>
2025-09-15 17:55:44 +0530

    Update connect.js

--- backend/connect/connect.js
+++ backend/connect/connect.js
@@ -1,30 +1,30 @@
 /* eslint-disable no-undef */
-// const mongoose=require("mongoose")
+const mongoose=require("mongoose")
 
-// const connectDB=(url)=>{
-//     return mongoose.connect(url)
-// }
-// module.exports=connectDB
-
-const { Pool } = require('pg')
-require('dotenv').config()
-
-const connectDB = async () => {
-    const pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    })
-    
-    try {
-        await pool.connect()
-        console.log('Connected to Supabase PostgreSQL!')
-        return pool
-    } catch (error) {
-        console.error('Database connection failed:', error)
-        throw error
-    }
+const connectDB=(url)=>{
+    return mongoose.connect(url)
 }
+module.exports=connectDB
 
-module.exports = connectDB
+// const { Pool } = require('pg')
+// require('dotenv').config()
+
+// const connectDB = async () => {
+//     const pool = new Pool({
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: {
+//             rejectUnauthorized: false
+//         }
+//     })
+    
+//     try {
+//         await pool.connect()
+//         console.log('Connected to Supabase PostgreSQL!')
+//         return pool
+//     } catch (error) {
+//         console.error('Database connection failed:', error)
+//         throw error
+//     }
+// }
+
+// module.exports = connectDB
