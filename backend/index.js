c5b47c3b6ad097df67dc16ef145212998be85c3c
Hrishikesh Chaudhari <chaudharihrishikesh30@gmail.com>
2025-09-16 11:01:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -78,9 +78,9 @@ app.use(cors(corsOptions));
 //   })
 // );
 // Enable preflight across all routes
-// app.options('*', cors(corsOptions));
+app.options('*', cors(corsOptions));
 
-// app.options('*', cors());
+app.options('*', cors());
 // for parsing data using res and request
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
